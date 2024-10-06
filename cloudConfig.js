442e071fb2e17bbb9415326d11d8bd9517589cae
ayush-1560 <ayush.15609514@gmail.com>
2024-10-06 16:01:31 +0530

    third commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
       folder: 'wanderlust_DEV',
-      allowedFormats: ["png","jpeg","jpg"],
+      allowedFormats: ["png","jpeg","jpg","avif"],
     },
   });
 
